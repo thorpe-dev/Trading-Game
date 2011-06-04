f28516814ea47a337ef7dbfa0c6f06400ac76f44
mt2309 <michael.thorpe09@imperial.ac.uk>
2011-06-04 01:00:32 +0100

    Files with character and move classes

---
+++ Character.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Collections.Generic;
+
+namespace Trading_Project
+{
+    abstract class Character
+    {
+        protected int health;
+
+
+        protected int strength;
+        protected int dexterity;
+        protected int speed;
+
+        protected List<Move> moves;
+    }
+}
