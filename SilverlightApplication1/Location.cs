54123c3d5dea2b21c0c942be01c571e0b3002139
MrClam <davidcoulden@hotmail.co.uk>
2011-06-09 16:40:20 +0100

    Added simple Location implementation

---
+++ SilverlightApplication1/Location.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace SilverlightApplication1
+{
+    public class Location
+    {
+        public int xcoord { get; set; }
+        public int ycoord { get; set; }
+        public LocationType place { get; set; }
+
+        public Location(LocationType t)
+        {
+            place = t;
+            xcoord = -1;
+            ycoord = -1;
+        }
+
+        public Location(LocationType t, int xcoord, int ycoord)
+        {
+            place = t;
+            this.xcoord = xcoord;
+            this.ycoord = ycoord;
+        }
+    }
+
+    public enum LocationType
+    {
+        HomeHub, Tavern, Shop, Dungeon
+    }
+}
